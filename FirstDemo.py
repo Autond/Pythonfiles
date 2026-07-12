8170c728f7b8ef91b8da943ac2218ad2527bf3d2
Autond <nehaawd2024@gmail.com>
2026-07-12 19:56:44 +0530

    My first python file

--- FirstDemo.py
+++ FirstDemo.py
@@ -2,4 +2,9 @@ print("hello")
 Str="hello world"
 print(Str)
 b,c,d=5,6.4,"Great"
-print("{} {}".format("value is",b))
+
+print("{} {}".format("value is",b)) # way to print string and integer together
+
+# to know type of variable
+
+print(type(b))
