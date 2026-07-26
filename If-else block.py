56366f4413576352f56373d5d59b002bc24c8ffd
Autond <nehaawd2024@gmail.com>
2026-07-26 18:47:00 +0530

    My first python file

---
+++ If-else block.py
@@ -0,0 +1,10 @@
+greeting ="Good Morning"
+
+if greeting == "Morning":
+    print("Condition Matches")
+    print("second line")
+
+else:
+    print("condition do not match")
+
+print("if else condition is completed")
