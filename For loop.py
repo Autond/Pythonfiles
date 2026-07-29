7af44307c9715e054f650c4c5d01581507b5f56a
Autond <nehaawd2024@gmail.com>
2026-07-29 18:03:53 +0530

    My first python file

--- For loop.py
+++ For loop.py
@@ -1,5 +1,10 @@
-obj = [2,3,5,7,9]
+#obj = [2,3,5,7,9]
 
-for i in obj:
-    print(i*2)
+#for i in obj:
+ #   print(i*2)
 
+# sum of first 5 natural numbers
+summation=0
+for j in range(1,6):
+    summation=summation+j
+print(summation)
