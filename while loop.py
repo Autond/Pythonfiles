922ba6d331d2c81d4df6841e652f8a7dcea97c3e
Autond <nehaawd2024@gmail.com>
2026-07-29 23:14:44 +0530

    My first python file

--- while loop.py
+++ while loop.py
@@ -2,7 +2,33 @@
 it=4
 
 while (it>1):
-    print(it)
+    if it!=3:
+        print(it)
     it=it-1
 
 print("while loop ended")
+print("*************break keyword->breaks the execution*************************")
+
+bt=5
+
+while bt>1:
+    if bt==3:
+        break
+
+    print(bt)
+    bt=bt-1
+print("*******continue-> stops that iteration only*******************************************")
+
+ct=10
+while ct>1:
+    if ct==9:
+        ct=ct-1
+        continue
+    if ct==3:
+        break
+    print(ct)
+    ct=ct-1
+
+
+
+
