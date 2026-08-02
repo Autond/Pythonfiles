91b20eebdf0a70cb854c3a94b12f9d6eba512774
Autond <nehaawd2024@gmail.com>
2026-08-02 19:40:17 +0530

    My first python file

---
+++ Functions.py
@@ -0,0 +1,14 @@
+#Function is a group of related statements that perform a specific task
+
+# Function declaration
+
+def GreetMe(name):
+    print("Good Morning"+" "+name)
+# Calling the function
+GreetMe("ABC")
+
+
+def AddIntegers(a,b):
+    print(a+b)
+
+AddIntegers(2,3)
